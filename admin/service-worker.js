80258fc8a0e37309585a395b7a19b0c1b6188a97
Zaiik <99999048+Zaiiik@users.noreply.github.com>
2026-09-03 15:17:46 +0200

    Add files via upload

---
+++ admin/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='leveling-admin-v3-1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./admin-icon-192.png','./admin-icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
