e8848b9c31274d96b79e03de30dd03954a7e595b
Zaiik <99999048+Zaiiik@users.noreply.github.com>
2026-09-14 19:12:50 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='leveling-app-v18-2-1';
+const CACHE='leveling-app-v18-2-2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
